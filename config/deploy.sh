cdd7c0fdc1205ad02bcd4df876c67c6ee6fb5fc4
aveok4now <aveok4now@gmail.com>
2024-10-13 17:39:20 +0300

    chore: update deploy.sh

--- config/deploy.sh
+++ config/deploy.sh
@@ -2,7 +2,7 @@
 
 cd docker
 echo "Starting containters..."
-docker-compose down
-docker-compose rm -f
-docker-compose up -d --build
-docker-compose ps
+docker compose down
+docker compose rm -f
+docker compose up -d --build
+docker compose ps
