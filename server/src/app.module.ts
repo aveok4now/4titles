263c15ef504854298114fb5e52cd11d54265a0e7
aveok4now <aveok4now@gmail.com>
2024-11-24 22:10:18 +0300

    chore: add graphql and redis support

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
+import { GraphQLModule } from '@nestjs/graphql'
+import { ApolloDriverConfig, ApolloDriver } from '@nestjs/apollo'
+import { join } from 'path'
+import { PlacesModule } from './places/places.module'
 
 @Module({
-    imports: [],
-    controllers: [AppController],
-    providers: [AppService],
+    imports: [
+        GraphQLModule.forRoot<ApolloDriverConfig>({
+            driver: ApolloDriver,
+            typePaths: ['./**/*.graphql'],
+            definitions: {
+                path: join(process.cwd(), 'src/graphql.ts'),
+            },
+            playground: true,
+        }),
+        PlacesModule,
+    ],
 })
 export class AppModule {}
