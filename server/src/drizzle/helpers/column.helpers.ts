906695120fce11b186874c694710fb83835d2bae
aveok4now <aveok4now@gmail.com>
2024-12-05 19:28:48 +0300

    chore: update timestamps column helpers

--- server/src/drizzle/helpers/column.helpers.ts
+++ server/src/drizzle/helpers/column.helpers.ts
@@ -1,6 +1,6 @@
 import { timestamp } from 'drizzle-orm/pg-core'
 
 export const timestamps = {
-    created_at: timestamp().defaultNow().notNull(),
-    updated_at: timestamp().defaultNow().notNull(),
+    createdAt: timestamp('created_at').defaultNow().notNull(),
+    updatedAt: timestamp('updated_at').defaultNow().notNull(),
 }
