aba9f1487dc436fab19218a4e3674e1980a4b89e
aveok4now <aveok4now@gmail.com>
2024-12-28 14:03:36 +0300

    fix: styles import in layout file

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import "../styles/globals.css";
 import { ApolloClientProvider } from "@/providers/ApolloClientProvider";
 
 const geistSans = Geist({
