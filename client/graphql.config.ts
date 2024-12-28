a833fcc4b04e9986f6953dadd74c75c8c5584b02
aveok4now <aveok4now@gmail.com>
2024-12-28 13:52:51 +0300

    chore: update graphql.config.ts

--- client/graphql.config.ts
+++ client/graphql.config.ts
@@ -1,7 +1,5 @@
 import type { CodegenConfig } from "@graphql-codegen/cli"
-import * as dotenv from 'dotenv'
-
-dotenv.config()
+import 'dotenv/config'
 
 const config: CodegenConfig = {
     schema: process.env.NEXT_PUBLIC_SERVER_URL,
