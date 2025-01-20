7f15f045a942d3fc537cf761cb5c7bbf791fd6c5
aveok4now <aveok4now@gmail.com>
2025-01-20 18:26:20 +0300

    feat: implement auth guards and decorators to retrieve current user from session

--- server/src/modules/auth/account/account.resolver.ts
+++ server/src/modules/auth/account/account.resolver.ts
@@ -1,15 +1,18 @@
+import { Authorization } from '@/shared/decorators/auth.decorator'
+import { Authorized } from '@/shared/decorators/authorized.decorator'
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql'
 import { AccountService } from './account.service'
-import { User } from './models/user.model'
 import { CreateUserInput } from './inputs/create-user.input'
+import { User } from './models/user.model'
 
 @Resolver(() => User)
 export class AccountResolver {
     constructor(private readonly accountService: AccountService) {}
 
-    @Query(() => [User])
-    async findAll(): Promise<User[]> {
-        return await this.accountService.findAll()
+    @Authorization()
+    @Query(() => User)
+    async user(@Authorized('id') id: string): Promise<User> {
+        return await this.accountService.findById(id)
     }
 
     @Mutation(() => Boolean)
--- server/src/modules/auth/account/account.service.ts
+++ server/src/modules/auth/account/account.service.ts
@@ -1,24 +1,23 @@
 import { DRIZZLE } from '@/modules/drizzle/drizzle.module'
 import { DbUser, users } from '@/modules/drizzle/schema/users.schema'
 import { DrizzleDB } from '@/modules/drizzle/types/drizzle'
+import { DatabaseException } from '@/modules/titles/exceptions/database.exception'
 import { DEFAULT_FETCH_LIMIT } from '@/modules/titles/services/constants/query.constants'
 import { ConflictException, Inject, Injectable } from '@nestjs/common'
-import { User } from './models/user.model'
-import { UserMapper } from './mappers/user.mapper'
-import { DatabaseException } from '@/modules/titles/exceptions/database.exception'
-import { CreateUserInput } from './inputs/create-user.input'
 import { hash } from 'argon2'
+import { CreateUserInput } from './inputs/create-user.input'
+import { UserMapper } from './mappers/user.mapper'
+import { User } from './models/user.model'
 
 @Injectable()
 export class AccountService {
     constructor(@Inject(DRIZZLE) private readonly db: DrizzleDB) {}
 
-    async findAll(): Promise<User[]> {
+    async findById(id: string): Promise<User | null> {
         try {
-            const dbUsers: DbUser[] = await this.db.query.users.findMany({
-                limit: DEFAULT_FETCH_LIMIT,
+            return await this.db.query.users.findFirst({
+                where: (users, { eq }) => eq(users.id, id),
             })
-            return UserMapper.toGraphQLList(dbUsers)
         } catch (error) {
             throw new DatabaseException(error)
         }
@@ -30,6 +29,17 @@ export class AccountService {
                 where: (users, { or, eq }) =>
                     or(eq(users.username, login), eq(users.email, login)),
             })
+        } catch (error) {
+            throw new DatabaseException(error)
+        }
+    }
+
+    async findAll(): Promise<User[]> {
+        try {
+            const dbUsers: DbUser[] = await this.db.query.users.findMany({
+                limit: DEFAULT_FETCH_LIMIT,
+            })
+            return UserMapper.toGraphQLList(dbUsers)
         } catch (error) {
             throw new DatabaseException(error)
         }
