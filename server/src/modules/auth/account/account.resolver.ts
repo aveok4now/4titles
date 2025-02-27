eff4f972430c1ed3ce3b858a39e62b17c0899c87
aveok4now <aveok4now@gmail.com>
2025-02-27 10:58:08 +0300

    feat: implement email and password change functionality

--- server/src/modules/auth/account/account.resolver.ts
+++ server/src/modules/auth/account/account.resolver.ts
@@ -2,6 +2,8 @@ import { Authorization } from '@/shared/decorators/auth.decorator'
 import { Authorized } from '@/shared/decorators/authorized.decorator'
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql'
 import { AccountService } from './account.service'
+import { ChangeEmailInput } from './inputs/change-email.input'
+import { ChangePasswordInput } from './inputs/change-password.input'
 import { CreateUserInput } from './inputs/create-user.input'
 import { User } from './models/user.model'
 
@@ -17,8 +19,26 @@ export class AccountResolver {
 
     @Mutation(() => Boolean)
     async createAccount(
-        @Args('input') input: CreateUserInput,
+        @Args('data') input: CreateUserInput,
     ): Promise<boolean> {
         return await this.accountService.create(input)
+    }
+
+    @Authorization()
+    @Mutation(() => Boolean)
+    async changeEmail(
+        @Authorized() user: User,
+        @Args('data') input: ChangeEmailInput,
+    ): Promise<boolean> {
+        return await this.accountService.changeEmail(user, input)
+    }
+
+    @Authorization()
+    @Mutation(() => Boolean)
+    async changePassword(
+        @Authorized() user: User,
+        @Args('data') input: ChangePasswordInput,
+    ): Promise<boolean> {
+        return await this.accountService.changePassword(user, input)
     }
 }
