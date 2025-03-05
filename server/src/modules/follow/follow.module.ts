b88352853cd7576bc865ec1409b1140798e40019
aveok4now <aveok4now@gmail.com>
2025-03-05 19:46:10 +0300

    chore: remove global decorator from account module

--- server/src/modules/follow/follow.module.ts
+++ server/src/modules/follow/follow.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common'
+import { AccountModule } from '../auth/account/account.module'
 import { FollowResolver } from './follow.resolver'
 import { FollowService } from './follow.service'
 
 @Module({
+    imports: [AccountModule],
     providers: [FollowResolver, FollowService],
 })
 export class FollowModule {}
