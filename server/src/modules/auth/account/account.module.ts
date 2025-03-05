46f4bc4f1d2adcbd18e91843a39d213bc7b54ca9
aveok4now <aveok4now@gmail.com>
2025-03-05 19:20:46 +0300

    chore: mage account module global

--- server/src/modules/auth/account/account.module.ts
+++ server/src/modules/auth/account/account.module.ts
@@ -1,11 +1,12 @@
 import { ContentModerationModule } from '@/modules/content-moderation/content-moderation.module'
 import { DrizzleModule } from '@/modules/drizzle/drizzle.module'
-import { Module } from '@nestjs/common'
+import { Global, Module } from '@nestjs/common'
 import { VerificationService } from '../verification/verification.service'
 import { AccountDeletionService } from './account-deletion.service'
 import { AccountResolver } from './account.resolver'
 import { AccountService } from './account.service'
 
+@Global()
 @Module({
     imports: [DrizzleModule, ContentModerationModule],
     providers: [
