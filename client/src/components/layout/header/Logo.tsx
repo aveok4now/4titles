dcae97b9a134ab28f87b3dab741fa1dad90811e1
aveok4now <aveok4now@gmail.com>
2025-04-23 14:50:56 +0300

    refactor: replace logo image with LogoImage component and remove logo SVG file

--- client/src/components/layout/header/Logo.tsx
+++ client/src/components/layout/header/Logo.tsx
@@ -2,9 +2,9 @@
 
 import { AuroraText } from '@/components/ui/custom/text/aurora-text'
 import ShinyText from '@/components/ui/custom/text/shiny-text'
+import { LogoImage } from '@/components/ui/elements/LogoImage'
 import { SITE_NAME } from '@/libs/constants/seo.constants'
 import { useTranslations } from 'next-intl'
-import Image from 'next/image'
 import Link from 'next/link'
 
 export function Logo() {
@@ -15,12 +15,7 @@ export function Logo() {
             href='/'
             className='flex items-center gap-x-4 transition-opacity duration-500 hover:opacity-70'
         >
-            <Image
-                src={'/images/logo.svg'}
-                alt={SITE_NAME}
-                width={40}
-                height={40}
-            />
+            <LogoImage />
             <div className='hidden flex-col leading-tight md:flex'>
                 <AuroraText
                     className='text-lg font-bold tracking-wider'
