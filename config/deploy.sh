7bd8de6f82de4bc0d116ba8c0bd0ec74d6debfe1
aveok4now <aveok4now@gmail.com>
2026-05-20 17:26:56 +0300

    chore: remove config directory

--- config/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd docker
-echo "Starting containters..."
-docker compose down
-docker compose rm -f
-docker compose up -d --build
-docker compose ps
--- config/download-models.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-mkdir -p models/mobilenet_v2
-git clone https://github.com/infinitered/nsfwjs.git temp
-cp -r temp/models/* models/mobilenet_v2/
-rm -rf temp
